e607b5a5dbc022fc1433fdeb604cb4d71224c674
derpcfreak <121813714+derpcfreak@users.noreply.github.com>
2026-01-26 16:55:23 +0100

    Update 430-firewall-rules.bash
    added dns fw rule

--- container/430-firewall-rules.bash
+++ container/430-firewall-rules.bash
@@ -10,8 +10,9 @@ fi
 
 sudo firewall-cmd --add-service=tftp --permanent
 sudo firewall-cmd --add-service=dhcp --permanent
+sudo firewall-cmd --add-service=dns --permanent
 sudo firewall-cmd --add-service=http --permanent
 sudo firewall-cmd --add-service=samba --permanent
 sudo firewall-cmd --add-masquerade --permanent
 sudo firewall-cmd --reload
-echo -e "[ ${LIGHTGREEN}OK${NC} ] Added firewall rules for tftp, dhcp, http and samba. Also masquerading is turned on"
+echo -e "[ ${LIGHTGREEN}OK${NC} ] Added firewall rules for tftp, dhcp, dns, http and samba. Also masquerading is turned on"
